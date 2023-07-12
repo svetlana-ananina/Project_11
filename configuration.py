fba95a582e1ecc99c609a1f432e09c144dfcbe29
svetlana-ananina <135277161+svetlana-ananina@users.noreply.github.com>
2023-07-12 12:33:47 +0300

    Add files via upload

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://506236e1-f438-495a-884e-abada18a82a3.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://34c984d8-71b7-4c7a-8e65-24c3ec3b45bb.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders"
 GET_ORDER_PATH = "/api/v1/orders/track?t="
--- create_order_test.py
+++ create_order_test.py
@@ -1,5 +1,9 @@
+# Светлана Ананьина, 6-я когорта — Финальный проект. Инженер по тестированию плюс
+# Тестирование API Яндекс Самокат 2.0
+
 import data
 import sender_stand_request
+
 
 # Создание заказа. Возвращает его трек
 def create_new_order():
@@ -7,6 +11,7 @@ def create_new_order():
     assert response.status_code == 201
     return response.json()["track"]
 
+# Тест создания и получения заказа в БД
 def test_create_and_get_new_order():
     track = create_new_order()
     response = sender_stand_request.get_order_info(str(track))
